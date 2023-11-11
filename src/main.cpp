da42e76325055aeb600c05c9bb7b6b0f3e33e5e4
Krzysztof <krzysztofdabkowski1@gmail.com>
2023-11-11 20:33:21 +0100

    Added constants to game

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 
 int main()
 {
+	const int TOWER_NUMBER = 3;
+	const int RINGS_NUMBER = 8;
+	const int FINAL_TOWER  = 3;
+
 	IAlgorithmResolver* resolver = new HanoiResolver();
-	Board board(4, 3, 3);
-	Game game(&board, resolver);
+	Board* board = new Board(TOWER_NUMBER, RINGS_NUMBER, FINAL_TOWER);
+	Game game(board, resolver);
 	game.run();
 	return 0;
 }
