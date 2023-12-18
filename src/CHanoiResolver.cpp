761f7a335a6c0193559740415f72c50cc5897408
Krzysztof <krzysztofdabkowski1@gmail.com>
2023-12-18 21:27:31 +0100

    develping

--- src/CHanoiResolver.cpp
+++ src/CHanoiResolver.cpp
@@ -50,22 +50,26 @@ void HanoiResolver::MakeNextStep(Board& _board)
                  tower[THIRD].topRingSize + 1 == tower[SECOND].topRingSize)
             moveRing(_board, THIRD, SECOND);    
         else if (tower[THIRD].isOrdered &&
+                 tower[SECOND].isOrdered &&
+                 tower[THIRD].size < 3 &&
                  tower[FIRST].topRingSize + 1 == tower[THIRD].topRingSize)
             moveRing(_board, FIRST, THIRD);
         else if (tower[FIRST].isOrdered &&
                  tower[SECOND].isOrdered &&
+                 tower[THIRD].size == 2 &&
                  tower[THIRD].isOrdered &&
                  tower[THIRD].baseRingSize + 1 == tower[SECOND].baseRingSize)
             moveRing(_board, THIRD, (tower[THIRD].size % 2 == 0) ? FIRST : SECOND);
         else if (tower[FIRST].isOrdered &&
                  tower[SECOND].isOrdered &&
                  tower[THIRD].isOrdered &&
+                 tower[SECOND].size == 2 &&
                  tower[SECOND].baseRingSize + 1 == tower[THIRD].baseRingSize)
             moveRing(_board, SECOND, (tower[SECOND].size % 2 == 0) ? FIRST : THIRD);
         else if (tower[SECOND].isOrdered &&
                  tower[SECOND].size > 1 &&
                  tower[SECOND].baseRingSize + 1 == tower[THIRD].baseRingSize)
-            moveRing(_board, SECOND, (tower[SECOND].size % 1 == 0) ? FIRST : THIRD);
+            moveRing(_board, SECOND, (tower[SECOND].size % 2 == 0) ? FIRST : THIRD);
         else if (tower[SECOND].isOrdered &&
                  tower[THIRD].size > 1 &&
                  tower[SECOND].size == 1 &&
@@ -75,222 +79,41 @@ void HanoiResolver::MakeNextStep(Board& _board)
                  tower[THIRD].isOrdered &&
                  tower[FIRST].baseRingSizeOfSubtower > 0 &&
                  tower[FIRST].baseRingSizeOfSubtower + 1 == tower[THIRD].topRingSize)
-            moveRing(_board, FIRST, (tower[FIRST].sizeOfSubtower % 1 == 0) ? SECOND : THIRD);
+            moveRing(_board, FIRST, (tower[FIRST].sizeOfSubtower % 2 == 0) ? SECOND : THIRD);
+
+        else
+        {
+            int sourceTower, destTower;
+            findSourceDestTower(sourceTower, destTower);
+            if (sourceTower == THIRD)
+            {
+                if (tower[SECOND].topRingSize != tower[SECOND].baseRingSize &&
+                    tower[FIRST].topRingSize  != tower[FIRST].baseRingSize  &&
+                    tower[SECOND].topRingSize + 1 == tower[FIRST].topRingSize)
+                    moveRing(_board, SECOND, FIRST);
+                else if (tower[SECOND].topRingSize != tower[SECOND].baseRingSize &&
+                    tower[FIRST].topRingSize  != tower[FIRST].baseRingSize  &&
+                    tower[FIRST].topRingSize + 1 == tower[SECOND].topRingSize)
+                    moveRing(_board, FIRST, SECOND);
+                else
+                    moveRing(_board, THIRD, (tower[THIRD].size % 2 == 0) ? FIRST : SECOND);
+            }
+            else if (sourceTower == FIRST && destTower == SECOND)
+            {
+                moveRing(_board, FIRST, (tower[FIRST].size % 2 == 0) ? SECOND : THIRD);
+            }
+            else if (sourceTower == SECOND && destTower == FIRST)
+            {
+                moveRing(_board, SECOND, (tower[SECOND].size % 2 == 0) ? FIRST : THIRD);
+            }
+            std::cout<<sourceTower<<std::endl;
+            std::cout<<destTower<<std::endl;
+        }
     }
 
 
 
     tower.clear();
-    // _numberOfTowers = _board.getTowerVector().size();
-    // int pointedTower = _board.getPointedTower();
-    
-    // std::vector<int> orderedTowers, unorderedTowers;
-    // _board.getOrderedTowers(orderedTowers, unorderedTowers);
-    // std::vector<std::pair<int,int>> sortedTowers;
-    // _board.getTowersSortedByBaseRing(sortedTowers);
-    // int lowTower   = sortedTowers.front().first;
-    // int largeTower = sortedTowers.back().first;
-    // if (orderedTowers.size() == 1)
-    // {
-    //     int tmpTower = _board.getTowerWithGreatestBaseRing();
-    //     int baseOfSubtower = _board.getBaseRingSizeOfSubtower(unorderedTowers[0]);
-    //     int baseOfSubtower2 = _board.getBaseRingSizeOfSubtower(unorderedTowers[1]);
-    //     std::cout<<baseOfSubtower<<std::endl;
-    //     std::cout<<baseOfSubtower2<<std::endl;
-    //     std::cout<<unorderedTowers[1]<<std::endl;
-    //     std::cout<<orderedTowers[0]<<std::endl;
-    //     if (baseOfSubtower > 1 && _board.getTowerVector()[lowTower]->topRingSize() == baseOfSubtower + 1 && _board.getTowerVector()[tmpTower]->topRingSize() == 1)
-    //     {
-    //         if (pointedTower != tmpTower)
-    //         {
-    //             _board.setPointedTower(tmpTower);
-    //             pointedTower = tmpTower;
-    //         }
-        
-    //         if (baseOfSubtower % 2 == 0)
-    //         {
-    //             _board.moveRingTo(unorderedTowers[1]);
-    //             _tmpBaseTower = unorderedTowers[1];  
-    //         }
-    //         else
-    //         {
-    //             _board.moveRingTo(orderedTowers[0]);
-    //             _tmpBaseTower = orderedTowers[0];                          
-    //         }
-    //     }
-    //     else if (!_board.getTowerVector()[unorderedTowers[1]]->isEmpty() && _board.getTowerVector()[tmpTower]->topRingSize() + 1 == _board.getTowerVector()[unorderedTowers[1]]->topRingSize())
-    //     {
-    //         if (pointedTower != tmpTower)
-    //         {
-    //             _board.setPointedTower(tmpTower);
-    //             pointedTower = tmpTower;
-    //         }
-    //         _board.moveRingTo(unorderedTowers[1]);
-    //         _tmpBaseTower = unorderedTowers[1];  
-    //     }
-    //     else if (baseOfSubtower2 + 1 == baseOfSubtower && baseOfSubtower2 != 0)
-    //     {
-    //         if (pointedTower != unorderedTowers[1])
-    //         {
-    //             _board.setPointedTower(unorderedTowers[1]);
-    //             pointedTower = unorderedTowers[1];
-    //         }
-
-    //         if (baseOfSubtower2 % 2 == 1)
-    //         {
-    //             _board.moveRingTo(unorderedTowers[0]);
-    //             _tmpBaseTower = unorderedTowers[0];  
-    //         }
-    //         else
-    //         {
-    //             _board.moveRingTo(orderedTowers[0]);
-    //             _tmpBaseTower = orderedTowers[0];                          
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if (pointedTower != tmpTower)
-    //         {
-    //             _board.setPointedTower(tmpTower);
-    //             pointedTower = tmpTower;
-    //         }
-
-    //         if (_board.getTowerVector()[tmpTower]->topRingSize() + 1 == _board.getTowerVector()[orderedTowers[0]]->topRingSize())
-    //         {
-    //             int destTower = _board.getTowerWithRingSize(_board.getTowerVector()[lowTower]->baseRingSize() + 1);
-    //             if (_board.getTowerVector()[destTower]->topRingSize() + 1 == _board.getTowerVector()[tmpTower]->topRingSize())
-    //             {
-    //                 if (pointedTower != destTower)
-    //                 {
-    //                     _board.setPointedTower(destTower);
-    //                     pointedTower = destTower;
-    //                 }
-    //                 _board.moveRingTo(tmpTower);
-    //                 _tmpBaseTower = tmpTower;
-    //             }
-    //             else
-    //             {
-    //                 _board.moveRingTo(orderedTowers[0]);
-    //                 _tmpBaseTower = orderedTowers[0];
-    //             }
-    //         }
-    //         else
-    //         {
-    //             _board.moveRingTo(unorderedTowers[0]);
-    //             _tmpBaseTower = unorderedTowers[0];
-    //         }       
-    //     }
-        
-    // }
-    // else if (orderedTowers.size() == 2)
-    // {
-    //     // orderedTowers.erase(std::remove(orderedTowers.begin(), orderedTowers.end(), lowTower), orderedTowers.end());
-    //     // orderedTowers.erase(std::remove(orderedTowers.begin(), orderedTowers.end(), largeTower), orderedTowers.end());
-    //     int baseOfSubtower = _board.getBaseRingSizeOfSubtower(unorderedTowers[0]);
-    //     int destTower = _board.getTowerWithRingSize(_board.getTowerVector()[lowTower]->baseRingSize() + 1);
-    //     int startTower = _board.getTowerWithGreatestBaseRing();
-    //     if (_board.getTowerVector()[destTower]->topRingSize() == _board.getTowerVector()[lowTower]->topRingSize() + 1  
-    //         && startTower != destTower)
-    //     {
-    //         if (pointedTower != lowTower)
-    //         {
-    //             _board.setPointedTower(lowTower);
-    //             pointedTower = lowTower;
-    //         }
-    //         _board.moveRingTo(destTower);
-    //         _tmpBaseTower = destTower;  
-    //     }
-    //     else if (_board.getTowerVector()[destTower]->topRingSize() + 1 == _board.getTowerVector()[lowTower]->topRingSize() 
-    //         && startTower != destTower)
-    //     {
-    //         if (pointedTower != lowTower)
-    //         {
-    //             _board.setPointedTower(lowTower);
-    //             pointedTower = lowTower;
-    //         }
-    //         _board.moveRingTo(startTower);
-    //         _tmpBaseTower = startTower;             
-    //     }
-    //     else if (baseOfSubtower > 0 && _board.getTowerVector()[destTower]->topRingSize() != _board.getTowerVector()[lowTower]->topRingSize() + 1)
-    //     {
-    //         if (pointedTower != lowTower)
-    //         {
-    //             _board.setPointedTower(lowTower);
-    //             pointedTower = lowTower;
-    //         }
-    //         _board.moveRingTo(orderedTowers[0]);
-    //         _tmpBaseTower = orderedTowers[0];
-    //     }
-    //     else if (baseOfSubtower > 0)
-    //     {
-    //         if (pointedTower != unorderedTowers[0])
-    //         {
-    //             _board.setPointedTower(unorderedTowers[0]);
-    //             pointedTower = unorderedTowers[0];
-    //         }
-
-    //         if (_board.getTowerVector()[unorderedTowers[0]]->topRingSize() + 1 == _board.getTowerVector()[orderedTowers[0]]->topRingSize())
-    //         {
-    //             _board.moveRingTo(orderedTowers[0]);
-    //             _tmpBaseTower = orderedTowers[0];
-    //         }
-    //         else if (_board.getTowerVector()[unorderedTowers[0]]->topRingSize() + 1 == _board.getTowerVector()[orderedTowers[1]]->topRingSize())
-    //         {
-    //             _board.moveRingTo(orderedTowers[1]);
-    //             _tmpBaseTower = orderedTowers[1];
-    //         }
-    //     }
-    //     else
-    //     {
-    //         if (pointedTower != startTower)
-    //         {
-    //             _board.setPointedTower(startTower);
-    //             pointedTower = startTower;
-    //         }
-    //         _board.moveRingTo(unorderedTowers[0]);
-    //         _tmpBaseTower = unorderedTowers[0];
-    //     }
-
-    // }
-    // else if (orderedTowers.size() == 3) 
-    // {
-    //     if (pointedTower != lowTower)
-    //     {
-    //         _board.setPointedTower(lowTower);
-    //         pointedTower = lowTower;
-    //     }
-
-    //     orderedTowers.erase(std::remove(orderedTowers.begin(), orderedTowers.end(), lowTower), orderedTowers.end());
-    //     orderedTowers.erase(std::remove(orderedTowers.begin(), orderedTowers.end(), largeTower), orderedTowers.end());
-    //     if( _board.getTowerVector()[lowTower]->hasOneRing())
-    //     {
-    //         _board.moveRingTo(orderedTowers[0]);
-    //         _tmpBaseTower = orderedTowers[0];
-    //     }
-    //     else
-    //     {
-    //         int destTower = _board.getTowerWithRingSize(_board.getTowerVector()[lowTower]->baseRingSize() + 1);
-    //         if (_board.getTowerVector()[destTower]->hasOneRing())
-    //         {
-    //             if (_board.getTowerVector()[lowTower]->size() % 2 == 1)
-    //             {
-    //                 _board.moveRingTo(destTower);
-    //                 _tmpBaseTower = destTower;  
-    //             }
-    //             else
-    //             {
-    //                 _board.moveRingTo(largeTower);
-    //                 _tmpBaseTower = largeTower;                          
-    //             }
-    //         }
-    //     }
-        
-    // }
-    // else
-    // {
-    //     int startTower = _board.getTowerWithGreatestBaseRing();
-    //     int baseOfSubtower = _board.getBaseRingSizeOfSubtower(startTower);
-    // }
 };
 
 unsigned int HanoiResolver::ResolveGame(Board& _board) 
@@ -353,13 +176,29 @@ void HanoiResolver::findSourceDestTower(int& _sourceTower, int& _destTower)
 {
     if (tower[SECOND].baseRingSize + 1 == tower[THIRD].baseRingSize)
     {
-        _sourceTower = THIRD;
-        _destTower   = SECOND;
-    }
-    else if (tower[THIRD].baseRingSize + 1 == tower[SECOND].baseRingSize)
-    {
         _sourceTower = SECOND;
-        _destTower   = THIRD;       
+        _destTower   = THIRD;
+    }
+    else if (tower[THIRD].baseRingSize + 1 == tower[SECOND].baseRingSize &&
+             tower[THIRD].size > 1)
+    {
+        _sourceTower = THIRD;
+        _destTower   = SECOND;       
+    }
+    else if (tower[THIRD].baseRingSize + 1 == tower[SECOND].baseRingSize && 
+             tower[THIRD].size == 1 &&
+             tower[SECOND].size > 1)
+    {
+        if (tower[FIRST].baseRingSizeOfSubtower== tower[SECOND].baseRingSizeOfSubtower + 1)
+        {
+            _sourceTower = SECOND;
+            _destTower   = FIRST;             
+        }
+        else if (tower[FIRST].baseRingSizeOfSubtower + 1 == tower[SECOND].topRingSize)
+        {
+            _sourceTower = FIRST;
+            _destTower   = SECOND;  
+        }     
     }
 
     if (tower[_sourceTower].baseRingSize > tower[FIRST].baseRingSize)
