192aac4ddbad11d430e8f88c5f963f6898cb3951
Krzysztof <krzysztofdabkowski1@gmail.com>
2024-01-14 09:54:49 +0100

    developing

--- src/CHanoiResolver.cpp
+++ src/CHanoiResolver.cpp
@@ -762,6 +762,18 @@ void HanoiResolver::MakeNextStep(Board& _board)
                 tower[FIRST].topRingSizeOfBaseTower == tower[THIRD].baseRingSizeOfSubtower + 1 &&
                 tower[FIRST].topRingSizeOfBaseTower + 1 == tower[SECOND].topRingSize)
             moveRing(_board, THIRD, (tower[THIRD].sizeOfSubtower % 2 == 0) ? SECOND : FIRST); 
+        else if(!tower[FIRST].isOrdered &&
+                !tower[SECOND].isOrdered &&
+                !tower[THIRD].isOrdered &&
+                tower[FIRST].sizeOfSubtower == 1 &&
+                tower[SECOND].sizeOfSubtower == 1 &&
+                tower[THIRD].sizeOfSubtower > 1 &&
+                tower[FIRST].topRingSize + 1 == tower[SECOND].topRingSize &&
+                tower[FIRST].topRingSizeOfBaseTower + 1 == tower[SECOND].topRingSizeOfBaseTower &&
+                tower[FIRST].topRingSizeOfBaseTower == tower[THIRD].baseRingSizeOfSubtower + 1 &&
+                tower[SECOND].topRingSize + 1 == tower[THIRD].topRingSize &&
+                tower[SECOND].topRingSizeOfBaseTower + 1 == tower[THIRD].topRingSizeOfBaseTower)
+            moveRing(_board, FIRST, SECOND); 
     }
 
 
