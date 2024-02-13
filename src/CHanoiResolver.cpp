aa3f22ec2a64356135399d582a2d81eca387dd18
Krzysztof <krzysztofdabkowski1@gmail.com>
2024-02-13 23:49:16 +0100

    developing

--- src/CHanoiResolver.cpp
+++ src/CHanoiResolver.cpp
@@ -1022,6 +1022,15 @@ void HanoiResolver::MakeNextStep(Board& _board)
                 tower[FIRST].baseRingSize + 1 == tower[SECOND].topRingSize &&
                 tower[SECOND].topRingSize + 1 == tower[THIRD].topRingSizeOfBaseTower)
             moveRing(_board, THIRD, FIRST);
+        else if(tower[FIRST].isOrdered &&
+                tower[SECOND].isOrdered &&
+                tower[THIRD].isOrdered &&
+                tower[FIRST].size > 1 &&
+                tower[SECOND].size == 1 &&
+                tower[THIRD].size == 1 &&
+                tower[FIRST].baseRingSize + 1 == tower[SECOND].topRingSize &&
+                tower[SECOND].topRingSize + 1 == tower[THIRD].topRingSize)
+            moveRing(_board, SECOND, THIRD);
     }
 
 
